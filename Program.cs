61da4d153bc4bc13d81192d685eaea69d5d9e311
Manuel Sasson <101148540+manusasson@users.noreply.github.com>
2022-07-25 21:32:11 -0300

    V2

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿public class Usuario
+{ 
+    public int id;
+    public string nombre;
+    public string apellido;
+    public string nombreUsuario;
+    public string contraseña;
+    public string mail;
+
+
+}
+
+
+public class Producto 
+
+{
+    public int id;
+    public string descripcion;
+    public double costo;
+    public double precioDeVenta;
+    public string stock;
+    public int idUsuario;
+
+}
+
+public class ProductoVendido
+
+{
+    public int id;
+    public int idProducto;
+    public int dtock;
+    public int idVenta;
+
+
+}
+
+public class Venta
+{
+    public int id;
+    public string Comentarios;
+}
